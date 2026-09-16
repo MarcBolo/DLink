a6d385f14e74783b3c80e2021a854b0a22ed9fa0
Marco Bolo <984848345@qq.com>
2026-09-17 00:44:26 +0800

    Delete electron.d.ts

--- electron.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'electron' {
-  interface Shell {
-    openPath(filePath: string): Promise<string>;
-    showItemInFolder(filePath: string): void;
-  }
-  export const shell: Shell;
-}
